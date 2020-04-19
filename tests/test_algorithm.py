59fdb5834552d71a60055afddca296f13474598a
Daniel <toreno96@gmail.com>
2020-04-19 16:09:38 +0200

    Feature/Implement TOTP (#2)

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+import dataclasses
+import datetime
 import unittest
 
 from onetimepass import algorithm
@@ -30,8 +34,75 @@ class TestHOTP(unittest.TestCase):
 
 
 class TestTOTP(unittest.TestCase):
+    # Based on https://tools.ietf.org/html/rfc6238#appendix-B
+
+    @dataclasses.dataclass
+    class TestTOTPParameters(algorithm.TOTPParameters):
+        secret: bytes = b"12345678901234567890"
+        digits_count: int = 8
+
+    @dataclasses.dataclass
+    class TestVector:
+        parameters: TestTOTPParameters
+        expected_counter: int
+        expected_totp: int
+
+    test_vectors = [
+        TestVector(
+            parameters=TestTOTPParameters(
+                current_time=datetime.datetime(
+                    1970, 1, 1, 0, 0, 59, tzinfo=datetime.timezone.utc
+                )
+            ),
+            expected_counter=int("1", 16),
+            expected_totp=94287082,
+        ),
+        TestVector(
+            parameters=TestTOTPParameters(
+                current_time=datetime.datetime(
+                    2005, 3, 18, 1, 58, 29, tzinfo=datetime.timezone.utc
+                )
+            ),
+            expected_counter=int("23523EC", 16),
+            expected_totp=7081804,
+        ),
+        TestVector(
+            parameters=TestTOTPParameters(
+                current_time=datetime.datetime(
+                    2009, 2, 13, 23, 31, 30, tzinfo=datetime.timezone.utc
+                )
+            ),
+            expected_counter=int("273EF07", 16),
+            expected_totp=89005924,
+        ),
+        TestVector(
+            parameters=TestTOTPParameters(
+                current_time=datetime.datetime(
+                    2033, 5, 18, 3, 33, 20, tzinfo=datetime.timezone.utc
+                )
+            ),
+            expected_counter=int("3F940AA", 16),
+            expected_totp=69279037,
+        ),
+        TestVector(
+            parameters=TestTOTPParameters(
+                current_time=datetime.datetime(
+                    2603, 10, 11, 11, 33, 20, tzinfo=datetime.timezone.utc
+                )
+            ),
+            expected_counter=int("27BC86AA", 16),
+            expected_totp=65353130,
+        ),
+    ]
+
     def test_to_hotp_parameters(self):
-        pass
+        for test_vector in self.test_vectors:
+            with self.subTest(i=test_vector.parameters.current_time):
+                counter = test_vector.parameters.to_hotp_parameters().counter
+                self.assertEqual(counter, test_vector.expected_counter)
 
     def test_totp(self):
-        pass
+        for test_vector in self.test_vectors:
+            with self.subTest(i=test_vector.parameters.current_time):
+                totp = algorithm.totp(test_vector.parameters)
+                self.assertEqual(totp, test_vector.expected_totp)
