d26ccf12b04d053172c15890bff7a6a6fd9f858f
Daniel <toreno96@gmail.com>
2020-08-08 01:31:34 +0200

    Configure pre-commit hooks (#5)
    * Configure pre-commit hooks
    
    * Use official pre-commit github action
    
    It comes with cache configured out-of-the-box.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 PACKAGE_NAME = "onetimepass"
 SCRIPT_NAME = "otp"
+
+DEPENDENCIES = ["click"]
+DEV_DEPENDENCIES = ["pre-commit"]
 
 setup(
     name=PACKAGE_NAME,
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click"],
-    extras_require={"dev": ["black"]},
+    install_requires=DEPENDENCIES,
+    extras_require={"dev": DEV_DEPENDENCIES},
     entry_points=f"""
         [console_scripts]
         {SCRIPT_NAME}={PACKAGE_NAME}.{SCRIPT_NAME}:otp
